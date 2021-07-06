88f4019ec666f9257a3f9ca22a7876925cdf97d8
thakuralok99 <thakuralok99@gmail.com>
2021-07-06 16:14:05 +0545

    edit navbar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,7 @@
-
+ {{-- fonts --}}
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,900;1,500&display=swap" rel="stylesheet">
 <!-- font awesome icons -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <!-- Styles -->
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,68 +1,74 @@
+<div class="container-fliud d-flex justify-content-center align-items-center">
+    <span>%OFF IN AUSTRALIA</span>
+</div>
+<div class="header">
     <!--top nav bar-->
+    <div>
+
+    
     <nav class="navbar navbar-expand-md navbar-dark color-second-bg shadow-sm">
-            <div class="container d-flex justify-content-between">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="d-flex m-0 p-0 align-items-center">
-                        <img src="{{asset('img/logo_gt.png')}}" style="width:3rem;" alt="">
-                        <a class="navbar-brand" href="{{ url('/') }}">GurkhaTrails</a>
-                    </ul>
+        <div class="container d-flex justify-content-between">
+            <!-- Left Side Of Navbar -->
+            <ul class="d-flex m-0 p-0 align-items-center">
+                <img src="{{asset('img/logo_gt.png')}}" style="width:3rem;" alt="">
+                <a class="navbar-brand" href="{{ url('/') }}">GurkhaTrails</a>
+            </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="d-flex m-0 p-0 list-unstyled align-items-center">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link text-light" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+            <!-- Right Side Of Navbar -->
+            <ul class="d-flex m-0 p-0 list-unstyled align-items-center">
+                <!-- Authentication Links -->
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link text-light" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a @if (Auth::user()->is_admin)
+                            href="{{route('dashboard')}}"
                         @else
-                            <li class="nav-item dropdown">
-                                <a @if (Auth::user()->is_admin)
-                                    href="{{route('dashboard')}}"
-                                @else
-                                    href="{{route('user.index')}}"
-                                @endif  class="nav-link text-light">
-                                    {{ Auth::user()->name }}
-                                </a>
-                            </li>
-                            <li>
-                                <a class="nav-link text-light" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </li>
-                        @endguest
-                            <li class="nav-item dropdown"> 
-                            @if (Cart::count())
-                                <a href="{{route('cart.index')}}" class="btn btn-light">cart- 
-                                    <span>{{Cart::count()}}</span>
-                                </a>
-                            @else
-                            <a href="{{route('cart.index')}}" class="nav-link text-light">cart </a>    
-                            @endif
-                            </li>
-                    </ul>
-                </div>
+                            href="{{route('user.index')}}"
+                        @endif  class="nav-link text-light">
+                            {{ Auth::user()->name }}
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link text-light" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </li>
+                @endguest
+                    <li class="nav-item dropdown"> 
+                    @if (Cart::count())
+                        <a href="{{route('cart.index')}}" class="btn btn-light">cart- 
+                            <span>{{Cart::count()}}</span>
+                        </a>
+                    @else
+                    <a href="{{route('cart.index')}}" class="nav-link text-light">cart </a>    
+                    @endif
+                    </li>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </ul>
             </div>
-        </nav>
-
+        </div>
+    </nav>
     <!--second navbar-->
-    <nav class="navbar navbar-expand-lg shadow-lg mb-3 navbar-light">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <nav class="navbar navbar-expand-md navbar-dark color-second-bg shadow-sm">
         <div class="collapse navbar-collapse " id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto ">
                 <li class="nav-item active">
@@ -101,13 +107,18 @@
                     </div>
                 </li>
             </ul>
+            <form class="form-inline ">
+                <div class="form-group mb-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                </div>
+                <div class="form-group mb-0">
+                    <button class="btn btn-sm btn-outline-success ml-2 my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+                </div>     
+            </form>
         </div>
-        <form class="form-inline ">
-            <div class="form-group mb-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            </div>
-            <div class="form-group mb-0">
-                <button class="btn btn-sm btn-outline-success ml-2 my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
-            </div>     
-        </form>
+        
     </nav>
+</div>
+</div>
+    
+   
